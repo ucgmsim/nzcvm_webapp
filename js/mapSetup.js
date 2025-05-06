e803910e9bb01253d2cd2f41bd32441e42fdd4fb
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2025-05-06 18:33:05 +1200

    leaflet graticule

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -1,15 +1,27 @@
-// filepath: /home/arr65/src/nzcvm_webapp/js/mapSetup.js
+import L from "leaflet";
+import AutoGraticule from "leaflet-auto-graticule";
 
 // Initialize map centered on New Zealand
-const map = L.map('map').setView([-41.2865, 174.7762], 6);
+export const map = L.map('map').setView([-41.2865, 174.7762], 6);
+window.map = map; // Make global for non-module scripts
 
 // Add tile layer (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Add auto-graticule
+new AutoGraticule().addTo(map);
+
 // Define initial parameters for the rectangle (used by rectangleControls)
-const initialOriginLat = -41.2865;
-const initialOriginLng = 174.7762;
-const initialExtentX = 300; // width in km
-const initialExtentY = 300; // height in km
+export const initialOriginLat = -41.2865;
+window.initialOriginLat = initialOriginLat; // Make global
+
+export const initialOriginLng = 174.7762;
+window.initialOriginLng = initialOriginLng; // Make global
+
+export const initialExtentX = 300; // width in km
+window.initialExtentX = initialExtentX; // Make global
+
+export const initialExtentY = 300; // height in km
+window.initialExtentY = initialExtentY; // Make global
